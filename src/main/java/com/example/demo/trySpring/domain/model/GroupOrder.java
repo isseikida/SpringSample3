7892afb0da87418cc32c26c4740cda15219c497c
isseikida <rasonbura8@gmail.com>
2019-11-20 23:55:28 +0900

    @Validatedアノテーションのパラメーターに実行順序のインターフェイスを設定

---
+++ src/main/java/com/example/demo/trySpring/domain/model/GroupOrder.java
@@ -0,0 +1,11 @@
+package com.example.demo.trySpring.domain.model;
+
+import javax.validation.GroupSequence;
+
+
+
+//NO.2243 : グループの実行順序
+@GroupSequence({ValidGroup1.class,ValidGroup2.class,ValidGroup3.class})
+public interface GroupOrder {
+
+}
