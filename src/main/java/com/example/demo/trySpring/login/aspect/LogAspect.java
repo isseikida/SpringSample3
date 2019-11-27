b9957c971f056d8b91563b088eb452150ac32ccf
isseikida <rasonbura8@gmail.com>
2019-11-27 22:40:27 +0900

    一部修正

--- src/main/java/com/example/demo/trySpring/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/trySpring/login/aspect/LogAspect.java
@@ -1,11 +1,11 @@
-package com.example.demo.trySpring.login.aspect;
-
-
-import org.springframework.stereotype.Component;
-
-
-@Aspect
-@Component
-public class LogAspect {
-
-}
+//package com.example.demo.trySpring.login.aspect;
+//
+//
+//import org.springframework.stereotype.Component;
+//
+//
+//@Aspect
+//@Component
+//public class LogAspect {
+//
+//}
