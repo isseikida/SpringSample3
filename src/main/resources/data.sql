1a8fd48a976e51f9429e90786252f0e7d7c57217
isseikida <rasonbura8@gmail.com>
2019-11-30 23:52:34 +0900

    一部修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO employee(employee_id,employee_firstname,employee_lastname,age,number
 VALUES(1,'茶谷','ひでじ',30,88);
 
 
---INSERT INTO m_user(user_id,password,user_name,birthday,age,marriage,role)
---VALUES('yamada@xxx.co.jp','password','山田太郎','1990-01-01',28,false,'ROLE_ADMIN');
+INSERT INTO m_user(user_id,password,user_name,birthday,age,marriage,role)
+VALUES('yamada@xxx.co.jp','password','山田太郎','1990-01-01',28,false,'ROLE_ADMIN');
