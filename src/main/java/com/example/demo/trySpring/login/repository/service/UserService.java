60c3c338efbfac800b0fd4c8a148a1da02385693
isseikida <rasonbura8@gmail.com>
2019-11-30 23:46:09 +0900

    サービスクラスにinsert用のメソッドを追加

--- src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
+++ src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.trySpring.login.repository.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.trySpring.login.domain.model.User;
 import com.example.demo.trySpring.login.repository.UserDao;
 
 
@@ -12,5 +13,23 @@ public class UserService {
 
 	@Autowired
 	UserDao dao;
+
+
+	//insert用メソッド
+	public boolean insert(User user) {
+
+		//insert実行
+		int rowNumber=dao.insertOne(user);
+
+		//判定用変数
+		boolean result=false;
+
+		if(rowNumber > 0) {
+			//insert成功
+			result=true;
+		}
+
+		return result;
+	}
 
 }
