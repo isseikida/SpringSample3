ff305de45caf743a98952f4120cac0d91f681ca2
isseikida <rasonbura8@gmail.com>
2019-11-30 23:53:27 +0900

    一部修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,13 +9,13 @@ CREATE TABLE IF NOT EXISTS employee(
 
 
 
---/* ユーザーマスタ */
---CREATE TABLE IF NOT EXISTS m_user(
---user_id VARCHAR(50)PRIMARY KEY,
---password VARCHAR(100),
---user_name VARCHAR(50),
---birthday DATE,
---age INT,
---marriage BOOLEAN,
---role VARCHAR(50)
---);
+/* ユーザーマスタ */
+CREATE TABLE IF NOT EXISTS m_user(
+user_id VARCHAR(50)PRIMARY KEY,
+password VARCHAR(100),
+user_name VARCHAR(50),
+birthday DATE,
+age INT,
+marriage BOOLEAN,
+role VARCHAR(50)
+);
