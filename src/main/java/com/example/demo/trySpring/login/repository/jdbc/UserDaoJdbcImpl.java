f2918ce592725577d6cddafb4513c4ac112d5795
isseikida <rasonbura8@gmail.com>
2019-12-11 01:05:42 +0900

    削除メソッドにSQL文を追加

--- src/main/java/com/example/demo/trySpring/login/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/example/demo/trySpring/login/repository/jdbc/UserDaoJdbcImpl.java
@@ -165,7 +165,11 @@ public class UserDaoJdbcImpl implements UserDao{
 	//Userテーブルを1件削除
 	@Override
 	public int deleteOne(String userId)throws DataAccessException{
-		return 0;
+
+		//1件削除
+		int rowNumber = jdbc.update("DELETE FROM m_user WHERE user_id=?",
+				userId);
+		return rowNumber;
 	}
 
 
