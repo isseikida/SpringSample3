886bae7cf953629a3c2173591a8fdc1e74a00112
isseikida <rasonbura8@gmail.com>
2019-12-13 22:35:28 +0900

    UserDaoJdbcImpl4を使えるようにする

--- src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
+++ src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
@@ -15,7 +15,7 @@ import com.example.demo.trySpring.login.repository.UserDao;
 public class UserService {
 
 	@Autowired
-	@Qualifier("UserDaoJdbcImpl3")
+	@Qualifier("UserDaoJdbcImpl4")
 	UserDao dao;
 
 
