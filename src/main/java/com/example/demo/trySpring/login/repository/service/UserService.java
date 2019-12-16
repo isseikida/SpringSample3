f46a0becc3372e677b0da157f0d5e6dd492332db
isseikida <rasonbura8@gmail.com>
2019-12-16 20:37:53 +0900

    userCsvOutメソッド呼び出し_ファイル取得メソッドの実装

--- src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
+++ src/main/java/com/example/demo/trySpring/login/repository/service/UserService.java
@@ -1,9 +1,16 @@
 package com.example.demo.trySpring.login.repository.service;
 
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.trySpring.login.domain.model.User;
@@ -15,7 +22,7 @@ import com.example.demo.trySpring.login.repository.UserDao;
 public class UserService {
 
 	@Autowired
-	@Qualifier("UserDaoJdbcImpl4")
+	@Qualifier("UserDaoJdbcImpl3")
 	UserDao dao;
 
 
@@ -100,7 +107,26 @@ public class UserService {
     }
 
 
+    //ユーザー一覧をCSVに出力する
+    //UserDaoJdbcImplの"userCsvOut"を呼び出し
+    public void userCsvOut() throws DataAccessException {
 
+    	//CSV出力
+    	dao.userCsvOut();
+    }
 
+    //サーバーに保存されているファイルを取得して、byte配列に変換する
+    public byte[] getFile(String fileName)throws IOException {
 
+    	//ファイルシステム(デフォルト)の取得
+    	FileSystem fs = FileSystems.getDefault();
+
+    	//ファイルを取得
+    	Path p = fs.getPath(fileName);
+
+    	//ファイルをbyte配列に変換
+    	byte[] bytes = Files.readAllBytes(p);
+
+    	return bytes;
+    }
 }
